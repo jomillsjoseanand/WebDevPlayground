9e2acbb2daefbf3f2e4a8bf514acb280e62eb7d2
jomillsjoseanand <44730936+jomillsjoseanand@users.noreply.github.com>
2022-02-02 14:59:02 -0500

    Reverted changes to the CSS files.

--- Carbonic/carbonic.js
+++ Carbonic/carbonic.js
@@ -11,7 +11,7 @@ const compound = {
     tally: 10,
     position: 5,
     display: () => {
-        if (compound.position > Math.floor(compound.tally/2)) {
+        if (compound.position > Math.floor(compound.tally / 2)) {
             compound.position = compound.tally - compound.position;
         }
         switch (compound.type) {
@@ -19,14 +19,14 @@ const compound = {
                 return 'CH₃-' + repeat(compound.tally - 2) + 'CH₃';
             case 'double':
                 if (compound.position === 1) {
-                    return 'CH₂=CH-' + repeat(compound.tally-3) + 'CH₃';
+                    return 'CH₂=CH-' + repeat(compound.tally - 3) + 'CH₃';
                 }
-                return 'CH₃-' + repeat(compound.position-2) + 'CH=CH-' + repeat(compound.tally - compound.position - 2) + 'CH₃';
+                return 'CH₃-' + repeat(compound.position - 2) + 'CH=CH-' + repeat(compound.tally - compound.position - 2) + 'CH₃';
             case 'triple':
                 if (compound.position === 1) {
-                    return 'CH≡C-' + repeat(compound.tally-3) + 'CH₃';
+                    return 'CH≡C-' + repeat(compound.tally - 3) + 'CH₃';
                 }
-                return 'CH₃-' + repeat(compound.position-2) + 'C≡C-' + repeat(compound.tally - compound.position - 2) + 'CH₃';
+                return 'CH₃-' + repeat(compound.position - 2) + 'C≡C-' + repeat(compound.tally - compound.position - 2) + 'CH₃';
         }
     }
 }
